f94de38bab4ea6ccfb12adde9a8c6f22aa9c8974
khanhhongtranle <khanhhong141049@gmail.com>
2021-06-10 11:04:37 +0700

    rider tranfer eth to driver

--- src/RiderConfirmScene.js
+++ src/RiderConfirmScene.js
@@ -3,6 +3,9 @@ import {Button} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
 import {makeStyles} from "@material-ui/core/styles";
 import {ProvidedInfoContext} from "./Provider/ProvidedInfoProvider";
+import {ContractContext} from "./Provider/ContractProvider";
+import Web3 from "web3";
+import {AccountContext} from "./Provider/AccountProvider";
 const useStyles = makeStyles((theme) => ({
     paper: {
         margin: theme.spacing(8, 4, 31, 4),
@@ -18,9 +21,21 @@ const useStyles = makeStyles((theme) => ({
 const RiderConfirmScene = (props) => {
     const history = useHistory()
     const classes = useStyles();
-    const {info, setInfo} = useContext(ProvidedInfoContext)
+    const {info, setInfo} = useContext(ProvidedInfoContext);
+    const {account, setAccount} = useContext(AccountContext);
+    const {contract, setContract} = useContext(ContractContext);
 
-    const handleConfirmButton = () => {
+    const handleConfirmButton =  () => {
+        const to = "0x3cA0eeda7EA86507D6ECD27c890EEfd7C8506DC8";
+        const from = account.address;
+
+        const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+        const value = web3.utils.toWei("10", "ether")
+        web3.eth.sendTransaction({from, to, value}, function(error, result) {
+            if(error) console.log(error)
+            else console.log(result)
+        });
+
         setInfo({})
         history.push('/confirm-ride-success')
     }
