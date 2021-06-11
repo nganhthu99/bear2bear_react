1de8ebb1d42a09e3b5b9ca271e227d4b5f5e9fe6
Thu Anh Nguyen <nganhthu99@gmail.com>
2021-06-11 22:18:26 +0700

    Add distance calculating for rider request scene

--- src/RiderRequestScene.js
+++ src/RiderRequestScene.js
@@ -1,9 +1,7 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField";
-import {ContractContext} from "./Provider/ContractProvider";
-import {AccountContext} from "./Provider/AccountProvider";
 import {useHistory} from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import {ProvidedInfoContext} from "./Provider/ProvidedInfoProvider";
@@ -11,6 +9,7 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import ChoosePositionDialog from "./ChoosePositionDialog";
 import {validatePhoneNumberUtil} from "./Utility/ValidateInputUtil";
 import FeedbackSnackbar from "./Common/FeedbackSnackbar";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -26,6 +25,9 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    progressBar: {
+        margin: theme.spacing(2, 0, 0, 0)
+    }
 }));
 
 const VEHICLE_TYPE = [
@@ -50,31 +52,34 @@ const RiderRequestScene = (props) => {
     const [vehicleType, setVehicleType] = useState("0")
     const [position, setPosition] = useState(null)
     const [destination, setDestination] = useState(null)
+    const [distance, setDistance] = useState(0)
 
-    const {account, setAccount} = useContext(AccountContext)
-    const {contract, setContract} = useContext(ContractContext)
     const {info, setInfo} = useContext(ProvidedInfoContext)
 
     const [error, setError] = useState(false)
+    const [isCalculating, setIsCalculating] = useState(false)
 
     const handleRequestButton = () => {
-        console.log(phoneNumber)
-        console.log(vehicleType)
-        console.log(position)
-        console.log(destination)
-
         if (validatePhoneNumberUtil(phoneNumber) && vehicleType && position && destination) {
             setInfo({
                 phoneNumber,
                 vehicleType,
                 position,
-                destination
+                destination,
+                distance
             })
             history.push('/list-drivers')
         } else {
             setError({severity: "warning", message: "Please fill out all fields!"})
         }
     }
+
+    useEffect(() => {
+        if (position && destination) {
+            setDistance(Number(destination) - Number(position));
+            setIsCalculating(false)
+        }
+    }, [position, destination])
 
     return (
         <div className={classes.paper}>
@@ -122,7 +127,10 @@ const RiderRequestScene = (props) => {
                             </InputAdornment>
                         )
                     }}
-                    onChange={(event) => {setPosition(event.target.value)}}
+                    onChange={(event) => {
+                        setIsCalculating(true)
+                        setPosition(event.target.value)
+                    }}
                 />
                 <TextField
                     variant="outlined"
@@ -137,8 +145,20 @@ const RiderRequestScene = (props) => {
                             </InputAdornment>
                         )
                     }}
-                    onChange={(event) => {setDestination(event.target.value)}}
-                />
+                    onChange={(event) => {
+                        setIsCalculating(true)
+                        setDestination(event.target.value)
+                    }}                />
+                 {!isCalculating &&
+                 <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Distance"
+                    value={`${distance} km`}
+                />}
+                {isCalculating && <LinearProgress className={classes.progressBar}/>}
                 <Button
                     type="submit"
                     fullWidth
